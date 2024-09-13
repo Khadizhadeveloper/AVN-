f16b8c68659019457cccf0d2f6564466276595cb
Khadizhadeveloper <hadyousha6@gmail.com>
2024-09-13 21:32:40 +0600

    Some updates

--- student/forms.py
+++ student/forms.py
@@ -4,22 +4,20 @@ from student.models import Student
 
 
 class StudentForm(forms.ModelForm):
-    first_name = forms.CharField(widget=forms.TextInput(attrs={
-        'class':'form-control',
-        'placeholder': 'First Name'}))
-    last_name = forms.CharField(widget=forms.TextInput(attrs={
-        'class':'form-control',
-        'placeholder': 'Last Name'}))
-    age=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Age'}))
-    email= forms.EmailField(widget=forms.EmailInput(attrs={
-        'class':'form-control',
-        'placeholder': 'Email',
-        'help_text':'A valid email address'}))
-    student_number= forms.CharField(widget=forms.TextInput(attrs={
-        'class':'form-control',
-        'placeholder': 'Student Number'}))
-    gender=forms.CharField(widget=forms.Select(attrs={'class':'form-control', 'placeholder':'Gender'}))
+    first_name = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}))
+    last_name = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'})
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={'class': 'form-control', 'placeholder': 'Email', 'help_text': 'A valid email address'})
+    )
+    student_number = forms.CharField(
+        widget=forms.TextInput(
+            attrs={'class': 'form-control', 'placeholder': 'Student Number'})
+    )
 
     class Meta:
         model = Student
-        fields = ['first_name','last_name','email','student_number']
+        fields = '__all__'
